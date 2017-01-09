c7fe601d7cf0648d3c71896e9471653b465b9e3f
Jared Hanson <jaredhanson@gmail.com>
2017-01-10 11:43:56 +1300

    Move stuff around.

--- plugins/auth/oob.js
+++ plugins/auth/oob.js
@@ -1,10 +1,10 @@
 exports = module.exports = {
-  'scheme': require('../../lib/auth/oob/scheme')
+  'scheme': require('../../lib/oob/auth/scheme')
 };
 
 exports.load = function(id) {
   try {
-    return require('../../lib/auth/oob/' + id);
+    return require('../../lib/oob/auth/' + id);
   } catch (ex) {
     if (ex.code == 'MODULE_NOT_FOUND') { return; }
     throw ex;
--- plugins/auth/otp.js
+++ plugins/auth/otp.js
@@ -1,10 +1,10 @@
 exports = module.exports = {
-  'scheme': require('../../lib/auth/otp/scheme')
+  'scheme': require('../../lib/otp/auth/scheme')
 };
 
 exports.load = function(id) {
   try {
-    return require('../../lib/auth/otp/' + id);
+    return require('../../lib/otp/auth/' + id);
   } catch (ex) {
     if (ex.code == 'MODULE_NOT_FOUND') { return; }
     throw ex;
