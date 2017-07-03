71b0c16f40dfca8326f836c1427e42f00ca3fe23
Jared Hanson <jaredhanson@gmail.com>
2017-07-03 14:31:49 -0700

    Add Auth0.

--- app/authentication/oob/verify.js
+++ app/authentication/oob/verify.js
@@ -1,9 +1,13 @@
-exports = module.exports = function(duoVerify) {
+exports = module.exports = function(auth0Verify, duoVerify) {
   
   return function(authenticator, txnID, cb) {
     switch (authenticator.vendor) {
+    case 'auth0':
+      return auth0Verify(authenticator, txnID, cb);
     case 'duo':
       return duoVerify(authenticator, txnID, cb);
+    default:
+      return cb(new Error('Unknown authenticator vendor'))
     }
   };
 };
@@ -12,5 +16,6 @@ exports['@implements'] = 'http://schemas.authnomicon.org/js/security/authenticat
 
 // // TODO: Make channels/vendors pluggable, and then remove this
 exports['@require'] = [
+  'http://schemas.authnomicon.org/js/login/mfa/opt/auth0/oob/verify',
   'http://schemas.authnomicon.org/js/login/mfa/opt/duo/oob/verify'
 ];
