e4ff29023ef07f14d79f1ab0be15523509f88f89
Jared Hanson <jaredhanson@gmail.com>
2017-11-08 17:26:29 +0100

    Add tests.

---
+++ test/authentication/otp/main.test.js
@@ -0,0 +1,38 @@
+/* global describe, it */
+
+var $require = require('proxyquire');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var factory = require('../../../app/authentication/otp/main');
+
+
+describe('authentication/otb/main', function() {
+  
+  it('should export factory function', function() {
+    expect(factory).to.be.a('function');
+  });
+  
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.equal('http://schemas.authnomicon.org/js/security/authentication/otp');
+  });
+  
+  describe('creating interface', function() {
+    var gateway = {
+      generate: function(){ return this; },
+      verify: function(){ return this; }
+    }
+    
+    var api = factory(gateway);
+    
+    it('should create API', function() {
+      expect(api.generate).to.be.a('function');
+      expect(api.verify).to.be.a('function');
+    });
+    
+    it('should bind context', function() {
+      expect(api.generate()).to.equal(gateway);
+      expect(api.verify()).to.equal(gateway);
+    });
+  }); // creating interface
+  
+});
