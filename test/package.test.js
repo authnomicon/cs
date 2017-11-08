cd6aaf893e43122bfea06ae4e13ab587ea7943c9
Jared Hanson <jaredhanson@gmail.com>
2017-11-08 16:26:29 +0100

    Package tests.

--- test/package.test.js
+++ test/package.test.js
@@ -1,13 +1,28 @@
 /* global describe, it */
 
-var pkg = require('..');
 var expect = require('chai').expect;
 
 
-describe('@authnomicon/mfa', function() {
+describe('@authnomicon/security', function() {
   
-  it('should export manifest', function() {
-    expect(pkg).to.be.an('object');
+  describe('package.json', function() {
+    var json = require('../package.json');
+    
+    it('should have assembly metadata', function() {
+      expect(json.assembly.namespace).to.equal('security');
+      
+      expect(json.assembly.components).to.have.length(4);
+      expect(json.assembly.components).to.include('authentication/oob/main');
+      expect(json.assembly.components).to.include('authentication/otp/main');
+      expect(json.assembly.components).to.include('authentication/otp/algorithms/hotp');
+      expect(json.assembly.components).to.include('authentication/otp/algorithms/totp');
+    });
+  });
+  
+  it('should throw if required', function() {
+    expect(function() {
+      var pkg = require('..');
+    }).to.throw(Error).with.property('code', 'MODULE_NOT_FOUND');
   });
   
 });
