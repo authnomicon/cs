9f6f672b713d681a8f2ddc8d6b9e1bd4352c9e5f
Jared Hanson <jaredhanson@gmail.com>
2017-11-08 16:04:22 +0100

    Simplify oob.

--- app/authentication/oob/main.js
+++ app/authentication/oob/main.js
@@ -1,23 +1,13 @@
-exports = module.exports = function(gateway, challenge, associate) {
+exports = module.exports = function(gateway) {
   
   return {
-    
-    associate: function(user, options, cb) {
-      console.log('OUT-OF-BAND ASSOCIATION');
-      console.log(user);
-      console.log(options);
-      
-    },  // associate
-    
-    challenge: challenge,
+    associate: gateway.associate.bind(gateway),
+    challenge: gateway.challenge.bind(gateway),
     verify: gateway.verify.bind(gateway)
-    
   };
 };
 
 exports['@implements'] = 'http://schemas.authnomicon.org/js/security/authentication/oob';
 exports['@require'] = [
-  './gateway',
-  'http://schemas.authnomicon.org/js/security/authentication/oob/challenge',
-  'http://schemas.authnomicon.org/js/security/authentication/oob/associate'
+  './gateway'
 ];
