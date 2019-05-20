98d9d74ea1d98e0bda15d28e9d230aff8a64a113
Jared Hanson <jaredhanson@gmail.com>
2019-05-20 15:16:55 -0700

    Simplify password api structure.

--- app/password/main.js
+++ app/password/main.js
@@ -1,11 +1,35 @@
-exports = module.exports = function(verify) {
+exports = module.exports = function(IoC, connect) {
   
-  return {
-    verify: verify
+  var modules = IoC.components('http://i.authnomicon.org/js/cs/IPasswordService')
+    , services = modules.map(function(m) { return m.a['@name']; });
+  
+  var api = {};
+  
+  // TODO: pass domain + etc in as options
+  // TODO: if realm is an argument, resolve it to a specific service, and proceed with that.
+  api.verify = function(username, password, options, cb) {
+    if (typeof options == 'function') {
+      cb = options;
+      options = undefined;
+    }
+    options = options || {};
+    
+    connect(services, function(err, conn) {
+      if (err) { return cb(err); }
+      
+      conn.verify(username, password, function(err, user) {
+        if (err) { return cb(err); }
+        if (!user) { return cb(null, false); }
+        return cb(null, user);
+      });
+    });
   };
+  
+  return api;
 };
 
 exports['@implements'] = 'http://schemas.authnomicon.org/js/cs/password';
 exports['@require'] = [
-  './verify'
+  '!container',
+  'http://i.bixbyjs.org/ns/connect'
 ];
