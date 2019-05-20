f05d9e4e3ab66a4d7cd71d116bf35c12d401b737
Jared Hanson <jaredhanson@gmail.com>
2019-05-20 15:09:02 -0700

    Remove unused requires.

--- app/password/verify.js
+++ app/password/verify.js
@@ -1,4 +1,4 @@
-exports = module.exports = function(IoC, connect, ns, services) {
+exports = module.exports = function(IoC, connect) {
   
   var modules = IoC.components('http://i.authnomicon.org/js/cs/IPasswordService')
     , services = modules.map(function(m) { return m.a['@name']; });
@@ -26,7 +26,5 @@ exports = module.exports = function(IoC, connect, ns, services) {
 
 exports['@require'] = [
   '!container',
-  'http://i.bixbyjs.org/ns/connect',
-  'http://i.bixbyjs.org/ns',
-  'http://i.bixbyjs.org/services'
+  'http://i.bixbyjs.org/ns/connect'
 ];
