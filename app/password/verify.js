db4d70a4a46ca5dcc2103fa64a97cc53d04698d7
Jared Hanson <jaredhanson@gmail.com>
2019-05-20 15:03:43 -0700

    Cleanup.

--- app/password/verify.js
+++ app/password/verify.js
@@ -1,64 +1,31 @@
-exports = module.exports = function(connect, ns, services) {
+exports = module.exports = function(IoC, connect, ns, services) {
   
-  return function(username, password, domain, cb) {
-    console.log('RESOLVE DOMAIN!');
-    console.log(domain)
-    
-    //var type = 'auth0-oauth2-ro'
-    var type = 'auth0-oauth2-token-password'
-    //var type = 'okta-password'
-    if (domain) {
-      type = [ type, domain ].join('.')
+  var modules = IoC.components('http://i.authnomicon.org/js/cs/IPasswordService')
+    , services = modules.map(function(m) { return m.a['@name']; });
+  
+  // TODO: pass domain + etc in as options
+  // TODO: if realm is an argument, resolve it to a specific service, and proceed with that.
+  return function(username, password, options, cb) {
+    if (typeof options == 'function') {
+      cb = options;
+      options = undefined;
     }
+    options = options || {};
     
-    console.log('RESOLVE TYPE: ' + type);
-    
-    connect([ type ], function(err, conn) {
+    connect(services, function(err, conn) {
       if (err) { return cb(err); }
-      console.log('WE ARE CONNECTED!');
-      console.log(conn);
       
       conn.verify(username, password, function(err, user) {
         if (err) { return cb(err); }
         if (!user) { return cb(null, false); }
         return cb(null, user);
       });
-      
     });
-    
-    
-    /*
-    ns.resolve(type, 'SRV', function(err, addrs) {
-      console.log(err);
-      console.log(addrs)
-      
-    });
-    */
-    
-    return;
-    
-    
-    var type = 'auth0-oauth2-ro';
-    services.createConnection(type, { url: url }, function() {
-      this.verify(username, password, function(err, user) {
-        if (err) { return cb(err); }
-        if (!user) { return cb(null, false); }
-        return cb(null, user);
-      });
-    });
-    
-    /*
-    var conn = sd.createConnection(type, { url: url });
-    conn.verify(username, password, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      return cb(null, user);
-    });
-    */
   };
 };
 
 exports['@require'] = [
+  '!container',
   'http://i.bixbyjs.org/ns/connect',
   'http://i.bixbyjs.org/ns',
   'http://i.bixbyjs.org/services'
